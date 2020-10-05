d8785bf2def50f22c5cd9089b6104d8943c19827
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-10-04 19:57:41 -0700

    Tried to jump the gun on learning gatsby. Burned myself a little

--- ChemApp/gatsby-node.js
+++ ChemApp/gatsby-node.js
@@ -4,4 +4,8 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const { Component } = require("react");
+
+// Testing this out
+// installed axios
+// may have jumped the gun here
