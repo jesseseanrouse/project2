ecd4c3c93ff33a135168a2365302a8fd0350ad6a
Jesse Rouse <jesserouse@apples-iMac-2.local>
2020-10-08 23:22:10 -0700

    a little proof reading

--- ChemApp/src/components/about/about.js
+++ ChemApp/src/components/about/about.js
@@ -1,35 +1,43 @@
 import React from "react"
-import './style.css'
+import "./style.css"
 
 const about = () => {
   return (
-    <main id='about'>
+    <main id="about">
       <h1>About</h1>
       <p>
-        The purpose of this app to help people who have trouble doing the math
-        while doing Chemistry. Its a subject that is hard enough to understand
-        without having to get caught up on the math. This is where this app
-        comes in. The goal is have you be able plug in some numbers and leave
-        the math to the app so you can understand whats going on without making
-        mistakes and causing more hurdles to jump over. While you still need to
-        be able to do the math at some point; this is here to help you get to
-        that point.
+        The purpose of this app is to provide math support while doing
+        Chemistry. Chemistry is a subject that is challenging to understand and
+        is further complicated by the necessity of precise math computation.
+        This is where this app comes in. The function of the app is to allow
+        persons to plug in compounds and elements, simplifying the math to
+        plugging in possible coefficients and leave the math calculations to the app so you can understand the concepts and relations within compounds.
       </p>
       <h1>How to use</h1>
       <p>
-        All you have to do is enter a compound or element in the space provided
-        and add it to either the Reactant or Product side of the equation.
-        Continue to do this until you have finish placing all of the compounds
-        on each side of the equation. The app will check to make sure that the
-        entry is valid before accepting it and give you a reason if it is
-        rejected.
+        To use the app, you enter a compound or element in the input box
+        provided and add it to either the Reactant or Product side of the
+        equation. Continue to do this until you have finish placing all of the
+        compounds on each side of the equation. The app will check to make sure
+        that the entry is valid before accepting it and give you a reason if an
+        entry is rejected.
       </p>
       <h1>Limitations</h1>
       <ul>
-        <li>This app only accepts letter, numbers, and parentheses*</li>
-        <li>You can only enter a number after an element and are limited to 999. This is for practical reasons; you really aren't going to come across an compound that has more that 999 elements in it.</li>
-        <li>Currently the balancing portion of this app is not working. This will be fix as soon as possible. If you change the numbers the site will crash.</li>
-        <li>*parentheses is currently not working. If you want to write a polyatomic ion, you will have to write it out for ion within the parentheses. Example: Ca(NO3)2 = CaNO3NO3 </li>
+        <li>This app only accepts letters, numbers, and parentheses*</li>
+        <li>
+          You can only enter a number after an element and are limited to 999.
+        </li>
+        <li>
+          Currently the balancing portion of this app is not working. This will
+          be fix as soon as possible. If you change the numbers the site will
+          crash.
+        </li>
+        <li>
+          *parentheses is currently not working. If you want to write a
+          polyatomic ion, you will have to write it out for ion within the
+          parentheses. Example: Ca(NO3)2 = CaNO3NO3{" "}
+        </li>
       </ul>
     </main>
   )
